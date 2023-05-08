5612264e8b8d4fc8ff9a40f6d298c6c723bd899a
Denis <Ori-wiki@gmail.com>
2023-05-08 19:06:54 +0300

    controllers users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 
-router.get('/users', (req, res) => {
-  res.send(users);
-});
+router.get('/users', () => {});
 router.get('/users/:id', () => {});
 router.post('/users, () => {}');
 
