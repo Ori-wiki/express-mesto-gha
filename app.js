f49b26eed9f1cef3d12f3815518c51901b01dd8c
Denis <Ori-wiki@gmail.com>
2023-05-08 17:13:15 +0300

    user schema

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+const mongoose = require('mongoose');
 const path = require('path');
 const express = require('express');
-const { setNoCacheHeaders } = require('./middlewares');
-
-const { PORT = 3000, BASE_PATH } = process.env;
+// const { setNoCacheHeaders } = require('./middlewares');
 const app = express();
+const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(setNoCacheHeaders);
+// app.use(setNoCacheHeaders);
 
 app.listen(PORT, () => {
-  console.log('Ссылка на сервер');
-  console.log(BASE_PATH);
+  console.log(`Ссылка на сервер ${PORT}`);
 });
