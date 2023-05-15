3a394dc9758728f8d18e123c0e900f8966d78be6
Denis <Ori-wiki@gmail.com>
2023-05-15 04:09:18 +0300

    apps

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const usersRoutes = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -9,8 +10,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(express.json());
 // подключаем логгер запросов
-
+app.use('/', usersRoutes);
 // роуты, которым нужна авторизация
 // app.use('/', require('./routes/users'));
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,20 @@
 const User = require('../models/user');
 
-module.exports.getUser = (req, res, next) => {
+const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch(next);
 };
-module.exports.getUserById = (req, res, next) => {
+const getUserById = (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => res.status(200).send(user))
     .catch(next);
 };
-module.exports.createUser = (req, res, next) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(next);
+const createUser = (req, res) => {
+  User.create(req.body).then((user) => res.status(201).send(user));
+  // .catch((e) =>
+  //   res.status(500).send({ message: `error creating user ${e}` })
+  // );
 };
+
+module.exports = { createUser, getUsers, getUserById };
