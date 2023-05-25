dee2f6b0bfdfd54a192195785313c0a1ac666174
Denis <Ori-wiki@gmail.com>
2023-05-25 21:25:39 +0300

    valid error

--- controllers/users.js
+++ controllers/users.js
@@ -37,7 +37,7 @@ const createUser = (req, res) => {
 };
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
         res.status(404).send({ message: 'Пользователь не найден' });
@@ -54,7 +54,7 @@ const updateProfile = (req, res) => {
 };
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
         res.status(404).send({ message: 'Пользователь не найден' });
