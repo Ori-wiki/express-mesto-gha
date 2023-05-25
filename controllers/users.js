4a881c6b12e39bac8fe9830f8a6b47b9c223fc0d
Denis <Ori-wiki@gmail.com>
2023-05-25 10:29:53 +0300

    404 status -> 400

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ const getUserById = (req, res) => {
   User.findById(id)
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: 'Пользователь не найден' });
+        res.status(400).send({ message: 'Пользователь не найден' });
       }
       res.status(200).send(user);
     })
