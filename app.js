9588c692a8055c31c6bca18457d64dfcc556a791
Denis <Ori-wiki@gmail.com>
2023-05-28 01:12:36 +0300

    ext

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.disable('x-powered-by');
