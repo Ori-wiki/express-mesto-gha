455ac155fc40eed1af571207ba390146c1b97fd3
Denis <Ori-wiki@gmail.com>
2023-05-28 00:31:46 +0300

    test fix

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const usersRoutes = require('./routes/users');
-const cardsRoutes = require('./routes/cards');
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { signUp, signIn } = require('./middlewares/validation');
-const NotFoundError = require('./errors/NotFoundError');
-
+// const usersRoutes = require('./routes/users');
+// const cardsRoutes = require('./routes/cards');
+// const { createUser, login } = require('./controllers/users');
+// const auth = require('./middlewares/auth');
+// const { signUp, signIn } = require('./middlewares/validation');
+// const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
+const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -21,21 +23,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(express.json());
-
+app.use(cors());
 app.use(helmet());
 app.disable('x-powered-by');
 
-app.post('/signup', signUp, createUser);
-app.post('/signin', signIn, login);
+app.use(requestLogger);
+app.use(router);
+app.use(errorLogger);
 
-app.use(auth);
-
-app.use('/', usersRoutes);
-app.use('/', cardsRoutes);
-
-app.use('*', (req, res, next) => {
-  next(new NotFoundError('Запрашиваемая страница не найдена'));
-});
+// app.use('*', (req, res, next) => {
+//   next(new NotFoundError('Запрашиваемая страница не найдена'));
+// });
 
 app.use(errors());
 app.use(errorHandler);
