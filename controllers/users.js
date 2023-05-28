29c21a24907551851f310d3cfbf311dcf9b2d11c
Denis <Ori-wiki@gmail.com>
2023-05-28 03:34:21 +0300

    jwt fix 1

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,7 @@ const MangoEmailError = require('../errors/MangoEmailError');
 const AuthError = require('../errors/AuthError');
 
 const { NODE_ENV, JWT_SECRET = 'dev-key' } = process.env;
+
 // NODE_ENV=production
 // JWT_SECRET=eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b
 
