bb6d90e06b723c93488506df6d682b19362b5d58
Denis <Ori-wiki@gmail.com>
2023-05-28 03:46:08 +0300

    test

--- routes/cards.js
+++ routes/cards.js
@@ -1,31 +1,3 @@
-// const router = require('express').Router();
-
-// const {
-//   getCards,
-//   createCards,
-//   deleteCard,
-//   likeCard,
-//   dislikeCard,
-// } = require('../controllers/cards');
-
-// const { createCardValidation, cardIdValidation } = require('../middlewares/validation');
-
-// const auth = require('../middlewares/auth');
-
-// router.use(auth);
-
-// router.get('/cards', getCards);
-
-// router.post('/cards', createCardValidation, createCards);
-
-// router.delete('/cards/:cardId', cardIdValidation, deleteCard);
-
-// router.put('/cards/:cardId/likes', cardIdValidation, likeCard);
-
-// router.delete('/cards/:cardId/likes', cardIdValidation, dislikeCard);
-
-// module.exports = router;
-
 const router = require('express').Router();
 const { createCardValidation, cardIdValidation } = require('../middlewares/validation');
 
